40a7e1d1a2040dc138c29fc51bbca33665823560
mhdianrush <mhdianrush@gmail.com>
2023-07-22 13:41:32 +0700

    Developed for Creating Real Time Log Monitoring Using os and logrus Package

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logger := logrus.New()
+	file, err := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		logger.Printf("failed created log file %s", err.Error())
+	}
+	logger.SetOutput(file)
+
+}

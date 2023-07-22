72aedb6fe0b15d81b3ddca38c9d11c7e667662f7
mhdianrush <mhdianrush@gmail.com>
2023-07-22 15:29:59 +0700

    Updated for Import app folder for Connecting to Server

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
+	"github.com/mhdianrush/go-ecommerce-api/app"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	r := mux.NewRouter()
 	logger := logrus.New()
 	file, err := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
@@ -19,12 +17,5 @@ func main() {
 
 	logger.Println("Server Running on Port 8080")
 
-	server := http.Server{
-		Addr:    ":8080",
-		Handler: r,
-	}
-	err = server.ListenAndServe()
-	if err != nil {
-		logger.Printf("failed connected to server %s", err.Error())
-	}
+	app.Run()
 }

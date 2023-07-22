5d21d08da8c254412ade448f51c552d71aa6deab
mhdianrush <mhdianrush@gmail.com>
2023-07-22 15:29:07 +0700

    Developed for Connecting to Server Using net/http Package, and Initializing Router Using Gorilla Mux

---
+++ app/server.go
@@ -0,0 +1,35 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+var logger = logrus.New()
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+func (server *Server) Initialize() {
+	server.Router = mux.NewRouter()
+}
+
+func (server *Server) Run(address string) {
+	logger.Printf("Server Running on Port %s", address)
+	err := http.ListenAndServe(address, server.Router)
+	if err != nil {
+		logger.Printf("failed connected to server %s", err.Error())
+	}
+}
+
+func Run() {
+	var server = Server{}
+
+	server.Initialize()
+	server.Run(":8080")
+}

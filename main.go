21bc125bd9e9e07a7d627d65c692adab2a8b69cb
mhdianrush <mhdianrush@gmail.com>
2023-07-22 13:47:55 +0700

    Developed for Initializing Router Using Gorilla Mux  and Connecting With Server Using nt/http Package

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	r := mux.NewRouter()
 	logger := logrus.New()
 	file, err := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
@@ -14,4 +17,14 @@ func main() {
 	}
 	logger.SetOutput(file)
 
+	logger.Println("Server Running on Port 8080")
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	err = server.ListenAndServe()
+	if err != nil {
+		logger.Printf("failed connected to server %s", err.Error())
+	}
 }

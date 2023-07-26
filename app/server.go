70459f1333c906fe6690257b89d7ac6055e04838
mhdianrush <mhdianrush@gmail.com>
2023-07-26 08:34:25 +0700

    feat(db connection): Opened PostgreSQL Database Connection

--- app/server.go
+++ app/server.go
@@ -1,12 +1,14 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -24,6 +26,17 @@ type AppConfig struct {
 }
 
 func (server *Server) Initialize() {
+	var err error
+	connStr := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+		os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_NAME"), os.Getenv("DATABASE_PORT"),
+	)
+
+	server.DB, err = gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	if err != nil {
+		logger.Printf("failed connected to database %s", err.Error())
+	}
+
 	server.Router = mux.NewRouter()
 	server.InitializeRoutes()
 }

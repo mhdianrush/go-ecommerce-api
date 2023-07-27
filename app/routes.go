cc87ca51db9b8f8e4167820279196bd7281ced81
mhdianrush <mhdianrush@gmail.com>
2023-07-27 21:33:02 +0700

    perf(InitializeRoutes): Updated InitializeRoutes Function, Added Initialize Router in This Function

--- app/routes.go
+++ app/routes.go
@@ -3,9 +3,11 @@ package app
 import (
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/mhdianrush/go-ecommerce-api/app/controllers"
 )
 
 func (server *Server) InitializeRoutes() {
+	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/", controllers.Home).Methods(http.MethodGet)
 }

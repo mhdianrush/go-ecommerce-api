1aa507f9c32e60e3489371d715f249c3457333bb
mhdianrush <mhdianrush@gmail.com>
2023-07-28 10:34:10 +0700

    Updated Product Struct

--- app/entities/product.go
+++ app/entities/product.go
@@ -11,15 +11,15 @@ type Product struct {
 	Id               string          `gorm:"primaryKey;not null;uniqueIndex;size:40"`
 	ParentId         string          `gorm:"size:40;index"`
 	UserId           string          `gorm:"size:40;index"`
-	Sku              string          `gorm:"size:150;index"`
+	Sku              string          `gorm:"size:200;index"`
 	Name             string          `gorm:"size:200"`
 	Slug             string          `gorm:"size:200"`
-	Price            decimal.Decimal `gorm:"type:decimal(16,2)"`
-	Stock            int
-	Weight           decimal.Decimal `gorm:"type:decimal(10,2)"`
 	ShortDescription string          `gorm:"size:200"`
 	Description      string          `gorm:"type:text"`
+	Price            decimal.Decimal `gorm:"type:decimal(16,2)"`
+	Weight           decimal.Decimal `gorm:"type:decimal(10,2)"`
 	Status           int             `gorm:"default:0"`
+	Stock            int
 	CreatedAt        time.Time
 	UpdatedAt        time.Time
 	DeletedAt        gorm.DeletedAt

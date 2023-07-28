967a916a94346b7267ad9f58005c35e7ba2760e0
mhdianrush <mhdianrush@gmail.com>
2023-07-28 10:29:54 +0700

    Updated Address Struct

--- app/entities/address.go
+++ app/entities/address.go
@@ -4,8 +4,8 @@ import "time"
 
 type Address struct {
 	Id         string `gorm:"primaryKey;not null;uniqueIndex;size:40"`
+	UserId     string `gorm:"size:40;index"`
 	FirstName  string `gorm:"size:200"`
-	IsPrimary  bool
 	Address1   string `gorm:"size:200"`
 	Address2   string `gorm:"size:200"`
 	CityId     string `gorm:"size:200"`
@@ -13,8 +13,8 @@ type Address struct {
 	Phone      string `gorm:"size:200"`
 	Email      string `gorm:"size:200"`
 	PostCode   string `gorm:"size:200"`
+	IsPrimary  bool
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 	User       User
-	UserId     string `gorm:"size:40;index"`
 }

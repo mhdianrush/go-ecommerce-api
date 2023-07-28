0ce692f026210e3749ff0d991e40e26a383f3014
mhdianrush <mhdianrush@gmail.com>
2023-07-28 09:37:22 +0700

    Created Payment Struct

---
+++ app/entities/payment.go
@@ -0,0 +1,27 @@
+package entities
+
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+	"gorm.io/gorm"
+)
+
+type Payment struct {
+	Id                   string          `gorm:"primaryKey;not null;uniqueIndex;size:40"`
+	OrderId              string          `gorm:"size:40;index"`
+	Number               string          `gorm:"size:200;index"`
+	Amount               decimal.Decimal `gorm:"type:decimal(16,2)"`
+	Method               string          `gorm:"size:200"`
+	Status               string          `gorm:"size:200"`
+	Token                string          `gorm:"size:200;index"`
+	Payload              string          `gorm:"type:text"`
+	PaymentType          string          `gorm:"size:200"`
+	VirtualAccountNumber string          `gorm:"size:200"`
+	BillCode             string          `gorm:"size:200"`
+	BillKey              string          `gorm:"size:200"`
+	CreatedAt            time.Time
+	UpdatedAt            time.Time
+	DeletedAt            gorm.DeletedAt
+	Order                Order
+}

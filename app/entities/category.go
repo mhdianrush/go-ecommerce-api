1675f0eafb0238e39c296c1bf2462091a32ad248
mhdianrush <mhdianrush@gmail.com>
2023-07-28 10:30:46 +0700

    Updated Category Struct

--- app/entities/category.go
+++ app/entities/category.go
@@ -4,12 +4,12 @@ import "time"
 
 type Category struct {
 	Id        string `gorm:"primaryKey;not null;uniqueIndex;size:40"`
+	SectionId string `gorm:"size:40;index"`
 	ParentId  string `gorm:"size:40"`
 	Name      string `gorm:"size:200"`
 	Slug      string `gorm:"size:200"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	Section   Section
-	SectionId string    `gorm:"size:40;index"`
 	Products  []Product `gorm:"many2many:product_categories"`
 }

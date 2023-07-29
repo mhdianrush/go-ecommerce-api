62691c5f6a88419958e0fde9cd9848daa7e52d70
mhdianrush <mhdianrush@gmail.com>
2023-07-29 20:20:49 +0700

    Created Seeder Struct, Developed RegisterSeeders to Registry All of Data, and Developed DatabaseSeed to Insert Data to Database

---
+++ database/seeders/seeder.go
@@ -0,0 +1,27 @@
+package seeders
+
+import (
+	"github.com/mhdianrush/go-ecommerce-api/database/fakers"
+	"gorm.io/gorm"
+)
+
+type Seeder struct {
+	Seeder any
+}
+
+func RegisterSeeders(db *gorm.DB) []Seeder {
+	return []Seeder{
+		{
+			Seeder: fakers.UserFaker(db),
+		},
+	}
+}
+
+func DatabaseSeed(db *gorm.DB) error {
+	for _, seeder := range RegisterSeeders(db) {
+		if err := db.Debug().Create(seeder.Seeder).Error; err != nil {
+			return err
+		}
+	}
+	return nil
+}

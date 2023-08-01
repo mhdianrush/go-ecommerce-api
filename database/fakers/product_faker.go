524a3e964ad35dd74e2be833e84ffe549583a4d9
mhdianrush <mhdianrush@gmail.com>
2023-08-01 21:31:27 +0700

    Developed for ProductFaker Function

---
+++ database/fakers/product_faker.go
@@ -0,0 +1,42 @@
+package fakers
+
+import (
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/bxcodec/faker/v4"
+	"github.com/google/uuid"
+	"github.com/gosimple/slug"
+	"github.com/mhdianrush/go-ecommerce-api/app/entities"
+	"github.com/shopspring/decimal"
+	"gorm.io/gorm"
+)
+
+func ProductFaker(db *gorm.DB) *entities.Product {
+	return &entities.Product{
+		Id:               uuid.New().String(),
+		UserId:           "",
+		Sku:              "",
+		Name:             faker.Name(),
+		Slug:             slug.Make(faker.Name()),
+		ShortDescription: faker.Paragraph(),
+		Description:      faker.Paragraph(),
+		Price:            decimal.NewFromFloat(fakePrice()),
+		Weight:           decimal.NewFromFloat(rand.Float64()),
+		Status:           1,
+		Stock:            rand.Intn(200),
+		CreatedAt:        time.Time{},
+		UpdatedAt:        time.Time{},
+		DeletedAt:        gorm.DeletedAt{},
+	}
+}
+
+func fakePrice() float64 {
+	return Precision(rand.Float64()*math.Pow10(rand.Intn(8)), rand.Intn(2)+1)
+}
+
+func Precision(val float64, pre int) float64 {
+	div := math.Pow10(pre)
+	return float64(int64(val*div)) / div
+}

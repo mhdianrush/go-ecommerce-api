f5fd5b712657de0502232a62eae07f04f3b18772
mhdianrush <mhdianrush@gmail.com>
2023-08-06 13:14:10 +0700

    Developed for Home Function to Show the Home Page

--- app/controllers/home.controller.go
+++ app/controllers/home.controller.go
@@ -1,10 +1,23 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"github.com/unrolled/render"
 )
 
+var logger = logrus.New()
+
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "ecommerce home page")
+	render := render.New(render.Options{
+		Layout: "layout",
+	})
+
+	if err := render.HTML(w, http.StatusOK, "home", map[string]any{
+		"title": "Home Page Title",
+		"body":  "Home Page Description",
+	}); err != nil {
+		logger.Printf("failed load html file %s", err.Error())
+	}
 }
